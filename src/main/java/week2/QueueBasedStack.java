b95da66cba35d94cbf84155296fa0a897cd79540
Avdo <avdo.vladavic@stu.ibu.edu.ba>
2024-03-07 14:53:39 +0100

    2nd week commit

--- src/main/java/week2/QueueBasedStack.java
+++ src/main/java/week2/QueueBasedStack.java
@@ -1,34 +1,64 @@
 package week2;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
 public class QueueBasedStack<Data> {
     private Queue<Data> q1;
     private Queue<Data> q2;
 
+    private Node<Data> top = null;
+    private int size;
+
+
+
+
     public QueueBasedStack() {
+        size=0;
         // your code here
+        q1=new Queue<>();
+        q2=new Queue<>();
     }
 
     public void push(Data data) {
+
         // your code here
+        while(!q1.isEmpty()){
+            Data temp= q1.dequeue();
+            q2.enqueue(temp);
+        }
+        q1.enqueue(data);
+        while (!q2.isEmpty()){
+            Data temp=q2.dequeue();
+            q1.enqueue(temp);
+        }
+
+        size++;
     }
 
     public Data pop() {
-        // your code here (remove next line)
-        return null;
+        if (isEmpty()) {													// 1
+            throw new NoSuchElementException("The stack is empty.");		// 1
+        }
+        Data item = q1.dequeue();
+
+        size--;															    // 4
+        return item;
+
     }
 
     public Data peek() {
         // your code here (remove next line)
-        return null;
+        return q1.peek();
     }
 
     public int size() {
         // your code here (remove next line)
-        return 0;
+        return size;
     }
 
     public boolean isEmpty() {
         // your code here (remove next line)
-        return false;
+        return size==0;
     }
 }
--- src/main/java/week2/TwoStackAlgorithm.java
+++ src/main/java/week2/TwoStackAlgorithm.java
@@ -1,8 +1,60 @@
 package week2;
 
-public class TwoStackAlgorithm {
+import javax.xml.crypto.Data;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collection.*;
+import java.util.Stack;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+public class TwoStackAlgorithm<Data> {
+    private static Stack<Double> s1;
+    private static Stack<Character> s2;
+
+
+
     public static Double calculate(String expression) {
         // your code here (remove next line)
-        return 0.0;
+
+
+        s1=new Stack<>();
+        s2=new Stack<>();
+        char[] chars=expression.toCharArray();
+        for(int i=0;i<chars.length;i++){
+
+            if(chars[i]>='0' && chars[i]<='9'){
+                s1.push((double) Character.getNumericValue(chars[i]));
+            }else if(chars[i]=='.'){
+                double temp=s1.pop();
+                s1.push(temp+chars[i+1]-temp);
+
+            } else if(chars[i]=='+' ||chars[i]=='-'||chars[i]=='*'||chars[i]=='/'||chars[i]=='%'||chars[i]=='^'||chars[i]=='√'){
+                s2.push(chars[i]);
+            } else if (chars[i]==')') {
+                double num1=s1.pop();
+                double num2=s1.pop();
+                char op=s2.pop();
+                switch (op){
+                    case '+' : s1.push(num1 + num2);
+                        break;
+                    case '-' : s1.push(num2 - num1);
+                        break;
+                    case '*' : s1.push(num2 *  num1);
+                        break;
+                    case '/' : s1.push(num1 /  num2);
+                        break;
+                    case '%' : s1.push(num1 % num2);
+                        break;
+                    case '^' : s1.push(pow(num1,num2));
+                        break;
+                    case '√' : s1.push(sqrt(num2));
+                        break;
+                }
+            }
+
+        }
+        return s1.pop();
     }
 }
