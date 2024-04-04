2dc3c575cb4d557dc7a28eb51f728080cbb6c155
Avdo <avdo.vladavic@stu.ibu.edu.ba>
2024-04-04 22:05:19 +0200

    6th week commit

--- src/main/java/week6/labTask/DualPivotQuickSort.java
+++ src/main/java/week6/labTask/DualPivotQuickSort.java
@@ -1,26 +1,65 @@
 package week6.labTask;
 
+import java.util.Random;
+
 public class DualPivotQuickSort {
 
-    /* Quick sort algorithm (public invocation) */
+
     public static <Data extends Comparable<Data>> void sort(Data[] elements) {
-        // your code here
+        shuffle(elements);
+        sort(elements, 0, elements.length - 1);
     }
 
-    /* Knuth shuffle (performance guarantee) */
+
     private static <Data extends Comparable<Data>> void shuffle(Data[] elements) {
-        // your code here
+        Random rand = new Random();
+        for (int i = 0; i < elements.length; i++) {
+            int r = i + rand.nextInt(elements.length - i);
+            swap(elements, i, r);
+        }
     }
 
-    /* Recursive quick sort logic */
+
     private static <Data extends Comparable<Data>> void sort(Data[] elements, int low, int high) {
-        // your code here
+        if (low < high) {
+            int[] pivots = partition(elements, low, high);
+            sort(elements, low, pivots[0] - 1);
+            sort(elements, pivots[0] + 1, pivots[1] - 1);
+            sort(elements, pivots[1] + 1, high);
+        }
     }
 
-    /* Partition an array using the dual-pivot approach and return the pivot indices */
+
     public static <Data extends Comparable<Data>> int[] partition(Data[] elements, int low, int high) {
-        // your code here (next line is a placeholder)
-        return null;
+        if (elements[low].compareTo(elements[high]) > 0) {
+            swap(elements, low, high);
+        }
+
+        Data leftPivot = elements[low];
+        Data rightPivot = elements[high];
+
+        int i = low + 1;
+        int j = high - 1;
+        int k = low + 1;
+
+        while (k <= j) {
+            if (elements[k].compareTo(leftPivot) < 0) {
+                swap(elements, k, i);
+                i++;
+            } else if (elements[k].compareTo(rightPivot) > 0) {
+                swap(elements, k, j);
+                j--;
+                k--;
+            }
+            k++;
+        }
+        i--;
+        j++;
+
+        swap(elements, low, i);
+        swap(elements, high, j);
+
+        return new int[]{i, j};
     }
 
     private static <Data extends Comparable<Data>> void swap(Data[] elements, int i, int j) {
