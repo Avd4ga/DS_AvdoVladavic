cbed4b693d370eb69ef637d0f773769471c26d89
Avdo <avdo.vladavic@stu.ibu.edu.ba>
2024-05-26 14:05:09 +0200

    Homework 2 commit

--- src/main/java/homework2/Process.java
+++ src/main/java/homework2/Process.java
@@ -2,10 +2,58 @@ package homework2;
 
 public class Process implements Comparable<Process> {
     // Implement the required attributes and constructor
+    private final String name;
+    private final int priority, arrival;
+    private int burst;
+    private int waitTime;
 
+    public Process(String name, int priority, int burst, int arrival) {
+        this.name = name;
+        this.priority = priority;
+        this.burst = burst;
+        this.arrival = arrival;
+        this.waitTime = - burst - arrival;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getBurst() {
+        return burst;
+    }
+
+    public int getArrival() {
+        return arrival;
+    }
+    public int getWaitTime(){
+        return waitTime;
+    }
+
+    public void setBurst(int burst){
+        this.burst = burst;
+    }
+    public void setWaitTime(int finishTime){
+        this.waitTime = finishTime + this.waitTime;
+    }
     @Override
     public int compareTo(Process other) {
         // implement the correct logic
-        return -1;
+        if(this.priority < other.priority){
+            return -1;
+        }
+        else if(this.priority > other.priority){
+            return 1;
+        }
+        else{
+            return 0;
+        }
     }
-}
+
+
+    }
+
--- src/main/java/homework2/Scheduler.java
+++ src/main/java/homework2/Scheduler.java
@@ -6,9 +6,54 @@ public class Scheduler {
 
     public static void scheduleAndRun(ArrayList<Process> processes) {
         // your code here
+        ProcessQueue pq = new ProcessQueue();
+        int t = 0;
+        int itemsLeft = processes.size();
+        float totalWaitTime = 0;
+        while(true){
+
+            String name = "No process";
+            for(Process p : processes){
+                if(p.getArrival()==t){
+                    pq.addProcess(p);
+                    itemsLeft--;
+                }
+            }
+            if(!pq.isEmpty()){
+
+                Process p = pq.peekNextProcess();
+                int burst = p.getBurst();
+
+                if(burst==1){
+                    p = pq.runNextProcess();
+                    p.setWaitTime(t+1);
+                    totalWaitTime += p.getWaitTime();
+                }
+
+                p.setBurst(--burst);
+                name = p.getName();
+            }
+
+            System.out.println("t: " + t + "   |   " + name);
+            t++;
+
+            if(pq.isEmpty() && itemsLeft==0){
+                System.out.println("Total time: " + t);
+                System.out.println("Average waiting time:" + (totalWaitTime/processes.size()));
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
         // you can use the main method for testing
+        ArrayList<Process> processes = new ArrayList<>();
+        processes.add(new Process("P1", 5, 4, 0));
+        processes.add(new Process("P2", 4, 3, 1));
+        processes.add(new Process("P3", 3, 1, 2));
+        processes.add(new Process("P4", 2, 5, 3));
+        processes.add(new Process("P5", 2, 2, 4));
+
+        scheduleAndRun(processes);
     }
 }
