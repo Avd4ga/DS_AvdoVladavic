3ac7e9fbfc3917b26ec0985c3f1640b50b9d50d4
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2024-06-05 09:44:18 +0200

    Added Homework 4 code.

---
+++ src/main/java/homework4/SocialNetwork.java
@@ -0,0 +1,34 @@
+package homework4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SocialNetwork {
+    private int V;
+    private int E;
+
+    private HashMap<String, ArrayList<Friendship>> adj;
+
+    public SocialNetwork() {
+        // implement the actual logic
+    }
+
+    public SocialNetwork(Scanner in) {
+        // implement the actual logic
+    }
+
+    public void addUser(String user) {
+        // implement the actual logic
+    }
+
+    public void addFriendship(Friendship f) {
+        // implement the actual logic
+    }
+
+    public ArrayList<FriendshipRecommendation> recommendFriends(String user) {
+        // implement the actual logic (remove next line)
+        return null;
+    }
+}
